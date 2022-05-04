c959067ad785313937c2adffbb11c64e4ba18ad4
Riul1999 <rodrigo.urrea@ing.uchile.cl>
2022-05-03 21:23:13 -0400

    feat: add gitignore, rename db and add new requeriments file

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("sqlite:///textPerimenter.sqlite")
+engine = create_engine("sqlite:///dashAI.sqlite")
 Session = sessionmaker(bind=engine)
 session = Session()
 
