4fd526d44e2e8d1b6a675ac0c2408904ffb7aa82
Maximiliano Aguilar <53657198+maximi4321@users.noreply.github.com>
2022-06-14 17:55:06 -0400

    retrieve user configuration for recursive parameters
    - Added a new optional parameter for gen_input to differentiate when it is generating a recursive parameter form.
    
    - updated store_parameters callback to   consider the recursive parameters form.
    
    - added a new callback function to retrieve the dropdown selection of a recursive parameter to return the form to display and configurate its parameters.

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from models import Experiment, Execution
 
 from Models.classes.getters import introspect_classes, filter_by_parent
 
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP],suppress_callback_exceptions=True)
+
 def parse_contents(contents, filename):
     """
     Loads the input data, if it's format is JSON, otherwise throw an exception
@@ -45,7 +47,7 @@ def get_task(task_type) -> Task:
     elif task_type == "TextClassificationMLabelTask":
         return TextClassificationMLabelTask()
 
-def gen_input(model_name : str, param_name : str, param_json_schema : dict):
+def gen_input(model_name : str, param_name : str, param_json_schema : dict, parent_model_data: dict = None):
     """
     Maps a parameter of a model to a dash input
     """
@@ -53,36 +55,44 @@ def gen_input(model_name : str, param_name : str, param_json_schema : dict):
     param_default = param_json_schema.get("default",None)
     input_component = None
 
+    id_dict = dict(type="form-input", name=f"{model_name}--{param_name}")
+    label_id_dict = dict(type="form-label", name=f"{model_name}--{param_name}")
+    if parent_model_data is not None:
+        id_dict["model"] = parent_model_data["model"]
+        id_dict["model_parameter"] = parent_model_data["parameter"]
+        label_id_dict["model"] = parent_model_data["model"]
+        label_id_dict["model_parameter"] = parent_model_data["parameter"]
+
     if param_type == "string":
         input_component = dcc.Dropdown(
-            id=dict(type='form-input', name=f"{model_name}-{param_name}"),
+            id=id_dict,
             options=[{'label':opt,'value':opt} for opt in param_json_schema.get("enum")],
             value=param_default
         )
 
     elif param_type == "boolean":
         input_component = dcc.Dropdown(
-            id=dict(type='form-input', name=f"{model_name}-{param_name}"),
+            id=id_dict,
             options=[{'label':opt, 'value':opt} for opt in ["True", "False"]],
             value=str(param_default)
         )
     elif param_type == "number":
         if "minimum" in param_json_schema.keys():
             input_component = dcc.Input(
-                id=dict(type='form-input', name=f"{model_name}-{param_name}"),
+                id=id_dict,
                 type="number",
                 min=param_json_schema.get("minimum"),
                 value=param_default
             )
         else:
             input_component = dcc.Input(
-                id=dict(type='form-input', name=f"{model_name}-{param_name}"),
+                id=id_dict,
                 type="number",
                 value=param_default
             )
     elif param_type == "integer":
         input_component = dcc.Input(
-            id=dict(type='form-input', name=f"{model_name}-{param_name}"),
+            id=id_dict,
             type="number",
             value=param_default,
             step=1,
@@ -90,31 +100,48 @@ def gen_input(model_name : str, param_name : str, param_json_schema : dict):
 
     elif param_type == "object":
         input_component = html.Div(
-            id=dict(type='form-input', name=f"{model_name}-{param_name}"),
-            children=[gen_input(model_name, param, param_json_schema.get("properties").get(param).get("oneOf")[0]) \
+            id=id_dict,
+            children=[gen_input(model_name, param, param_json_schema.get("properties").get(param).get("oneOf")[0], parent_model_data= parent_model_data) \
                 for param in param_json_schema.get("properties").keys()]
         )
     
     elif param_type == "class":
         parent_class_name = param_json_schema.get("parent")
-        input_component = dcc.Dropdown(
-            id=dict(type='form-input', name=f"{model_name}-{param_name}"),
-            options=[{'label':opt, 'value':opt} for opt in filter_by_parent(parent_class_name,introspect_classes()).keys()],
-            value=str(param_default)
+        f = open(f'Models/parameters/models_schemas/tweetTok.json')
+        f2 = open(f'Models/parameters/models_schemas/normalTok.json')
+        input_component = html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        dcc.Dropdown(
+                            id=dict(type = 'recursive-parameter-dropdown', name=f"{model_name}--{param_name}"),
+                            options=[{'label':opt, 'value':opt} for opt in filter_by_parent(parent_class_name,introspect_classes()).keys()],
+                            value=str(param_default)
+                        )
+                    ]
+                ),
+                dbc.Accordion(
+                    id = dict(type="recursive-parameter-accordion", name=f"{model_name}--{param_name}"),
+                    children =[
+                        #dbc.AccordionItem([gen_input("tweetTok", "tweetTok", json.load(f), parent_model_data={"model":model_name, "parameter": param_name})], title="tweetTok parameters"),
+                        #dbc.AccordionItem([gen_input("normalTok", "normalTok", json.load(f2), parent_model_data={"model":model_name, "parameter": param_name})], title="normalTok parameters")
+                    ],
+                    flush=True
+
+                )
+            ]
         )
 
     return html.Div(
         id=f"{model_name}-{param_name}-div",
         children=[
-            html.Label(f"{param_name}: ", id=dict(type='form-label', name=f"{model_name}-{param_name}")),
+            html.Label(f"{param_name}: ", id=label_id_dict),
             input_component
         ]
     )
 
 ###################################################################################
 ###################################################################################
-
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP],suppress_callback_exceptions=True)
 
 app.layout = html.Div([
     dbc.Row([
@@ -175,14 +202,16 @@ app.layout = html.Div([
                     style={'display':'none'}),
                 html.Div(id='parameter-config'),
                 html.Br(),
-                dbc.Button("Submit", color="dark", className="me-1", id='button-submit', style={'display': 'none'}),
+                dbc.Button("Save", color="dark", className="me-1", id='button-submit', style={'display': 'none'}),
                 html.Br(),
                 dbc.Button("Run Experiment", color="dark", className="me-1", n_clicks=0, id='button-run-experiment', style={'display': 'none'})
             ]),
         #gen_input("SVM", "SVM", json.load(f)),
         html.Br(),
         # Experiment Results
-        html.Div(id='experiment-results')
+        html.Div(id='experiment-results'),
+        #dbc.Button("asdfasdf", color="dark", className="me-1", id='button-1'),
+        html.Div(id='test-test', children=[html.H2()])
         ])
     ])
     ]),
@@ -197,6 +226,31 @@ app.layout = html.Div([
     dcc.Store(id='params-dict')
 ])
 
+
+@app.callback(
+    Output(dict(type="recursive-parameter-accordion", name=ALL), "children"),
+    [Input(dict(type='recursive-parameter-dropdown', name=ALL), "value"),
+    Input(dict(type='recursive-parameter-dropdown', name=ALL), "id")],
+    prevent_initial_call = True
+)
+def display_recursive_parameter_form(rec_parameter_selected_options, rec_parameter_dropdown_ids):
+    """
+    Get the selected option for all recursive parameters dropdowns and display
+    the forms to configure each one of them.
+    """
+    #dictionary to map the names from the dropdown and the names in the json schema
+    tokenizer_map = {"NormalTokenizer": "normalTok", "TweetTokenizer": "tweetTok"}
+
+    forms = []
+    for option, id in zip(rec_parameter_selected_options, rec_parameter_dropdown_ids):
+        mapped_option = tokenizer_map[option]
+        f = open(f'Models/parameters/models_schemas/{mapped_option}.json')
+        name = id["name"].split("--")
+
+        accordion_item_form = dbc.AccordionItem([gen_input(mapped_option, mapped_option, json.load(f), parent_model_data={"model":name[0], "parameter": name[1]})], title=f"{mapped_option} parameters")
+        forms.append(accordion_item_form)
+    return forms
+
 @app.callback(
     Output('params-dict', 'data'),
     Output('test-parameters', 'children'),
@@ -204,9 +258,19 @@ app.layout = html.Div([
     Input('executions', 'value')],
     [State(dict(type='form-input', name=ALL), 'value'),
     State(dict(type='form-label', name=ALL), 'children'),
+    State(dict(type='form-input', model=ALL, model_parameter=ALL, name=ALL), 'value'),
+    State(dict(type='form-label', model=ALL, model_parameter=ALL, name=ALL), 'children'),
+    State(dict(type='form-input', model=ALL, model_parameter=ALL, name=ALL), 'id'),
     State('params-dict', 'data')],
     prevent_initial_call=True)
-def store_parameters(n_clicks, executions, values, labels, prev_params_dict):
+def store_parameters(n_clicks,
+                     executions,
+                     values,
+                     labels,
+                     rec_params_values,
+                     rec_params_labels,
+                     rec_params_ids,
+                     prev_params_dict):
     """
     Get and store default parameters when a model is selected.
     Retrieve and store parameters configured by the user.
@@ -229,12 +293,43 @@ def store_parameters(n_clicks, executions, values, labels, prev_params_dict):
         params_dict = {}
         for label, value in zip(labels[1:], values[1:]):
             params_dict[label[:-2]] = value
+        output_params_dict  = {}
         if prev_params_dict is None:
-            return {labels[0][:-2] : params_dict}, str({labels[0][:-2] : params_dict})
+             output_params_dict = {labels[0][:-2] : params_dict}
         else:
             prev_params_dict[labels[0][:-2]] = params_dict
-            return prev_params_dict, str(prev_params_dict)
-    
+            output_params_dict =  prev_params_dict
+        
+        #recursive parameters
+
+        rec_parameters_dict = {}
+        rec_param_name = rec_params_labels[0][:-2]
+        model = rec_params_ids[0]["model"]
+        param_name_in_model = rec_params_ids[0]["model_parameter"]
+
+        for id, value, label in zip(rec_params_ids, rec_params_values, rec_params_labels):
+            name = id["name"].split("--")
+            #this condition refers to when "gen_input" creates a label for the model
+            #as it was another parameter, then "model_name" (name[0]) has the same
+            #value as "param_name" (name[1]) and that is used to mark the first parameter
+            #of a recursive parameter form.
+            if len(name) == 2 and name[0] == name[1]:
+                #if rec_parameters_dict is not an empty dict ({})
+                if rec_parameters_dict:
+                    output_params_dict[model][param_name_in_model] = {"value": rec_param_name, 'parameters': rec_parameters_dict}
+                
+                #resets the values to add the next recursive parameter form.
+                rec_parameters_dict = {}
+                rec_param_name = name[0]
+                continue
+
+            rec_parameters_dict[label[:-2]] = value
+        
+        #last recursive parameter form is never added in the previous for loop so it needs
+        #to be added after the for loop ends.
+        output_params_dict[model][param_name_in_model] = {"value": rec_param_name, 'parameters': rec_parameters_dict}
+
+        return output_params_dict, str(output_params_dict)
     
     
  
