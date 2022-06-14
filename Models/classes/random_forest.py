ff8f7c12cc24472cdc4952fa11d31d279d6818fb
Riul1999 <rodrigo.urrea@ing.uchile.cl>
2022-06-14 13:27:30 -0400

    feat: add two new vectors classifiers to the app

---
+++ Models/classes/random_forest.py
@@ -0,0 +1,30 @@
+from sklearn.ensemble import RandomForestClassifier
+from Models.classes.model import Model
+
+import json
+from io import BytesIO
+import joblib
+
+class RandomForest(Model, RandomForestClassifier):
+    """
+    """
+    MODEL = "randomforest"
+    TASK = ["TextClassificationSimpleTask"]
+    with open(f'Models/parameters/models_schemas/{MODEL}.json') as f:
+        SCHEMA = json.load(f)
+    
+    def save(self, filename=None):
+
+        if filename is None:
+            bytes_container = BytesIO()
+            joblib.dump(self, bytes_container)
+            bytes_container.seek(0)
+            return bytes_container.read()
+        else:
+            joblib.dump(self, filename)
+
+    @staticmethod
+    def load(filename):
+
+        model = joblib.load(filename)
+        return model
