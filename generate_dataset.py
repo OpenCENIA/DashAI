7a3248adff431a1dad9a5fc5e7a93cadccae56f8
Ignacio Núñez <ignacionunez@MacBook-Air-de-Ignacio-2.local>
2022-06-19 01:32:17 -0400

    add: Iris and IMDB datasets

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,11 +1,13 @@
 import csv, json
 
-csvFilePath = "Example_dataset/Twitter_Data.csv"
-jsonFilePath = "Example_dataset/twitterDataset.json"
+csvFilePath = "Example_dataset/IMDB_Sentiment.csv"
+jsonFilePath = "Example_dataset/ImdbSentimentDataset.json"
 
-size = 1000
+size = 100
+every = 4 # Every 'every' train registers, one is assigned to test dataset
 
 data = {
+    "task_info":{"task_type":"TextClassificationTask"},
     'train': {'x':[], 'y':[]},
     'test': {'x':[], 'y':[]}
 }
@@ -16,20 +18,15 @@ with open(csvFilePath, encoding="utf8") as csvFile:
     p = 0
     for row in csvReader:
         try:
-            actual_data = [row['clean_text'], int(row['category'])]
+            actual_data = [row['\ufefftext'], int(row['label'])]
         except:
             continue
 
-        if actual_data[1] == 0:
-            continue
-
-        if c == size:
-            c = 0
-            p += 1
-        if p == 0 :
+        if c%(every+1) != 0:
             data['train']['x'].append(actual_data[0])
             data['train']['y'].append(actual_data[1])
-        elif p == 1:
+
+        elif c%(every+1) == 0:
             data['test']['x'].append(actual_data[0])
             data['test']['y'].append(actual_data[1])
         else:
@@ -38,3 +35,85 @@ with open(csvFilePath, encoding="utf8") as csvFile:
 
 with open(jsonFilePath, 'w') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4))
+
+###############################################################################
+
+# csvFilePath = "Example_dataset/Iris.csv"
+# jsonFilePath = "Example_dataset/irisDataset.json"
+
+# size = 100
+# every = 4 # Every 'every' train registers, one is assigned to test dataset
+
+# data = {
+#     "task_info":{"task_type":"NumericClassificationTask"},
+#     'train': {'x':[], 'y':[]},
+#     'test': {'x':[], 'y':[]}
+# }
+
+# with open(csvFilePath, encoding="utf8") as csvFile:
+#     csvReader = csv.DictReader(csvFile)
+#     c = 0
+#     p = 0
+#     for row in csvReader:
+#         try:
+#             actual_data = [[float(row[i]) for i in ["SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm"]], row['Species']]
+#         except:
+#             continue
+
+#         if actual_data[1] == 0:
+#             continue
+
+#         if c%(every+1) != 0:
+#             data['train']['x'].append(actual_data[0])
+#             data['train']['y'].append(actual_data[1])
+
+#         elif c%(every+1) == 0:
+#             data['test']['x'].append(actual_data[0])
+#             data['test']['y'].append(actual_data[1])
+#         else:
+#             break
+#         c += 1
+
+# with open(jsonFilePath, 'w') as jsonFile:
+#     jsonFile.write(json.dumps(data, indent=4))
+
+###############################################################################
+
+# csvFilePath = "Example_dataset/Twitter_Data.csv"
+# jsonFilePath = "Example_dataset/twitterDataset.json"
+
+# size = 1000
+
+# data = {
+#     'train': {'x':[], 'y':[]},
+#     'test': {'x':[], 'y':[]}
+# }
+
+# with open(csvFilePath, encoding="utf8") as csvFile:
+#     csvReader = csv.DictReader(csvFile)
+#     c = 0
+#     p = 0
+#     for row in csvReader:
+#         try:
+#             actual_data = [row['clean_text'], int(row['category'])]
+#         except:
+#             continue
+
+#         if actual_data[1] == 0:
+#             continue
+
+#         if c == size:
+#             c = 0
+#             p += 1
+#         if p == 0 :
+#             data['train']['x'].append(actual_data[0])
+#             data['train']['y'].append(actual_data[1])
+#         elif p == 1:
+#             data['test']['x'].append(actual_data[0])
+#             data['test']['y'].append(actual_data[1])
+#         else:
+#             break
+#         c += 1
+
+# with open(jsonFilePath, 'w') as jsonFile:
+#     jsonFile.write(json.dumps(data, indent=4))
